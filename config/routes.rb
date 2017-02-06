d3a4c802a97fcdca05d327644bcf32025eb0fb94
Jennifer Schafer <jennifer.schafer@avantcredit.com>
2017-02-05 21:15:37 -0600

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :welcomes
-    root 'welcome#index'
+    root 'welcomes#index'
   resources :encounters
   resources :people
   # The priority is based upon order of creation: first created -> highest priority.
