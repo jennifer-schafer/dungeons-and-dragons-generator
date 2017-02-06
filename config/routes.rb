47ff7264248355112e4a639b155a778cdf2fc432
Jennifer Schafer <jennifer.schafer@avantcredit.com>
2017-02-05 22:46:55 -0600

    testing encounters now

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :people
 
    get 'people' => 'people#view'
+   get 'encounters' => 'encounters#view'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
