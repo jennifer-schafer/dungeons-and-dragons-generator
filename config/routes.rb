3a4aa20de7e0006a963d630b395155226e46961b
Jennifer Schafer <jennifer.schafer@avantcredit.com>
2017-02-06 19:44:18 -0600

    removing gets from route

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   resources :encounters
   resources :people
 
-   get 'people' => 'people#view'
-   get 'encounters' => 'encounters#view'
+  # get 'people' => 'people#view'
+  # get 'encounters' => 'encounters#view'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
